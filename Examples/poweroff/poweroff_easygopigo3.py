62696553709ab458fc9ea0ec1d183357f5423f36
slowrunner <slowrunner@users.noreply.github.com>
2022-01-31 10:33:31 -0500

    removed motor_float const - in gopigo3 base class

--- Examples/poweroff/poweroff_easygopigo3.py
+++ Examples/poweroff/poweroff_easygopigo3.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 
 
-"""
-   FILE:  poweroff_easygopigo3.py
 
-   PURPOSE: Test current/power difference of issuing a set_motor_power(-128) 
-            to totally remove power flowing to motors
+#   FILE:  poweroff_easygopigo3.py
 
-"""
+#   PURPOSE: Test current/power difference of issuing a set_motor_power(-128)
+#            to totally remove power flowing to motors
+
 
 from time import sleep
 from easygopigo3 import EasyGoPiGo3
 
-# imports for test
+# imports for test main
 import sys
-sys.path.insert(1,'/home/pi/GoPiLgc/plib')
+sys.path.append("/home/pi/GoPiLgc/plib")
 import speak
 
 
 class My_EasyGoPiGo3(EasyGoPiGo3):
-    MOTOR_FLOAT = -128
-
 
     def __init__(self, config_file_path="/home/pi/Dexter/gpg3_config.json", use_mutex=True):
-        super().__init__(config_file_path=config_file_path)
+        super().__init__(config_file_path=config_file_path, use_mutex=use_mutex)
 
     def stop_with_power_off(self):
         self.set_motor_dps(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
@@ -39,74 +36,74 @@ def main():  # TEST
 
     stmt="GO PI GO 3 POWER-OFF TEST"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SLEEPING FOR 1 MINUTE TO SETTLE LOAD"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(60)
 
     stmt="BEFORE ANY DRIVE COMMANDS"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SLEEPING FOR 5 MINUTES"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(300)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
 
     # First use motors to have them
     stmt="CAUTION, ABOUT TO MOVE FORWARD WITH drive_cm(10)"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(5)
     megpg.drive_cm(10)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SLEEPING FOR 5 MINUTES"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(300)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SENDING POWER-OFF TO MOTORS"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     megpg.stop_with_power_off()
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SLEEPING FOR 5 MINUTES"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(300)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
 
     # Now use motors with forward command 
     stmt="CAUTION, ABOUT TO MOVE for 1 second with forward() command"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(5)
 
     megpg.forward()
@@ -115,40 +112,40 @@ def main():  # TEST
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SLEEPING FOR 5 MINUTES"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(300)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SENDING POWER-OFF TO MOTORS"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     megpg.stop_with_power_off()
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
     stmt="SLEEPING FOR 5 MINUTES"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
     sleep(300)
 
     stmt="Please record current and power now"
     print(stmt)
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
 
 
     stmt="TEST COMPLETE"
     print("***",stmt,"***")
-    speak.say(stmt)
+    speak.whisper(stmt,anytime=True)
 
 
 if __name__ == "__main__": main()
