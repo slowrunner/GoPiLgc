b6580edb6478583481fb381112aade073775396f
slowrunner <slowrunner@users.noreply.github.com>
2022-03-12 11:10:38 -0500

    Read at 4 times per second

--- Examples/first_vscode/robot2.py
+++ Examples/first_vscode/robot2.py
@@ -10,7 +10,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(funcName)s: %(message)s')
 
-DIODE_DROP = 0.81
+DIODE_DROP = 0.7
 ULTRASONIC_CORRECTION_AT_100mm = 17.0 # mm
 ToF_CORRECTION_AT_100mm = -5.0 # mm 
 
@@ -23,10 +23,10 @@ def main():
         try:
             vBatt = egpg.volt()+DIODE_DROP
             dist_ds_mm = egpg.ds.read_mm()+ToF_CORRECTION_AT_100mm
-            time.sleep(0.1)
+            time.sleep(0.01)
             dist_us_mm = egpg.us.read_mm()+ULTRASONIC_CORRECTION_AT_100mm
             logging.info(": vBatt:{:>5.2f}v  ds:{:>5.0f}mm  us:{:>5.0f}mm".format(vBatt,dist_ds_mm,dist_us_mm))
-            time.sleep(1)
+            time.sleep(0.075)
 
         except KeyboardInterrupt:
             print("\nExiting...")
